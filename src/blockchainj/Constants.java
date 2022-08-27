22d527f027aedeaa5322934dcfbacd80b68d5bff
JAN ISTOCKO <istocko@gmail.com>
2022-08-27 10:02:47 +0200

    initial release

---
+++ src/blockchainj/Constants.java
@@ -0,0 +1,7 @@
+package blockchainj;
+
+public class Constants {
+	public static final int DIFFICULTY =5;
+	public static final double MINER_REWARD = 500;
+	public static final String GENESIS_PREV_HASH = "0000000000000000000000000000000000000000000000000000000000000000"; 
+}
